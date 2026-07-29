e302ad7a44ae70809b1354dd46a3e61e3e11b6bb
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-07-29 22:20:23 +0530

    [Pankaj] Add. Perimeter of a Rectangle

---
+++ Rectangle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Rectangle{
+   public static void main(String [] args) {
+       Scanner sc = new Scanner(System.in);
+       double length = sc.nextDouble();
+       double width = sc.nextDouble();
+       double perimeter = 2 * (length + width);
+       System.out.println("Celsius to Fahrenheit Conversion is " + perimeter);
+   }
+}
