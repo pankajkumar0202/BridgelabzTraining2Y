1ca30de40edb4dba58dab82c71f2c355e52e81b1
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-07-29 22:12:30 +0530

    [Pankaj] Add. volume program

---
+++ Volume.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Volume{
+   public static void main(String [] args) {
+       Scanner sc = new Scanner(System.in);
+       double r = sc.nextDouble();
+       double h = sc.nextDouble();
+       double volume = 3.14 * r*r * h;
+       System.out.println("Celsius to Fahrenheit Conversion is " + volume);
+   }
+}
