de795b3f8fef76da143af9dc34c9ca641b374fac
Pankaj Kumar Rajput <pankaj476987@gmail.com>
2026-07-29 22:07:42 +0530

    [Pankaj] Add. area of circle

---
+++ Area.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Area{
+   public static void main(String [] args) {
+       Scanner sc = new Scanner(System.in);
+       double r = sc.nextDouble();
+       double area = 3.14 * r*r;
+       System.out.println("Celsius to Fahrenheit Conversion is " + area);
+   }
+}
